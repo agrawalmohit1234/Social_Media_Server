dbf509b2c29b4fd09317a4930f8765a82ddf9898
unknown <rajagrawalmohit2@gmail.com>
2021-09-28 09:47:40 +0530

    Change in Error

--- server/helpers/dbErrorHandler.js
+++ server/helpers/dbErrorHandler.js
@@ -1,35 +1,35 @@
-'use strict'
+"use strict";
 const getUniqueErrorMessage = (err) => {
-    let output
-    try {
-        let fieldName =
-            err.message.substring(err.message.lastIndexOf('.$') + 2,
-                err.message.lastIndexOf('_1'))
-        output = fieldName.charAt(0).toUpperCase() + fieldName.slice(1) + ' already exists'
-    } catch (ex) {
-        output = 'Unique field already exists'
-    }
-    return output
-}
+  let output;
+  try {
+    let fieldName = err.message.substring(
+      err.message.lastIndexOf(".$") + 2,
+      err.message.lastIndexOf("_1")
+    );
+    output = fieldName.slice(1) + " already exists";
+  } catch (ex) {
+    output = "Unique field already exists";
+  }
+  return output;
+};
 
 const getErrorMessage = (err) => {
-    let message = ''
-    if (err.code) {
-        switch (err.code) {
-            case 11000:
-            case 11001:
-                message = getUniqueErrorMessage(err)
-                break
-            default:
-                message = 'Something went wrong'
-        }
-    } else {
-        for (let errName in err.errors) {
-            if (err.errors[errName].message)
-                message = err.errors[errName].message
-        }
+  let message = "";
+  if (err.code) {
+    switch (err.code) {
+      case 11000:
+      case 11001:
+        message = getUniqueErrorMessage(err);
+        break;
+      default:
+        message = "Something went wrong";
     }
-    return message
-}
+  } else {
+    for (let errName in err.errors) {
+      if (err.errors[errName].message) message = "Something went wrong";
+    }
+  }
+  return message;
+};
 
-module.exports = { getErrorMessage }
+module.exports = { getErrorMessage };
