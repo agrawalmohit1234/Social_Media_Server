41e7f139c465c0b219be2f960a68e9a84ac073e9
unknown <rajagrawalmohit2@gmail.com>
2021-09-28 09:52:16 +0530

    Change in Error

--- server/helpers/dbErrorHandler.js
+++ server/helpers/dbErrorHandler.js
@@ -2,11 +2,7 @@
 const getUniqueErrorMessage = (err) => {
   let output;
   try {
-    let fieldName = err.message.substring(
-      err.message.lastIndexOf(".$") + 2,
-      err.message.lastIndexOf("_1")
-    );
-    output = fieldName.slice(1) + " already exists";
+    output = "email already exists";
   } catch (ex) {
     output = "Unique field already exists";
   }
